d14b88deb536e1678b238143ba743c05dd32be62
kallu <lucas.aries@sfr.fr>
2021-03-14 20:16:43 +0100

    sample

---
+++ src/main/kotlin/DemoMain.kt
@@ -0,0 +1,15 @@
+/** main launch the app*/
+fun main() {
+    println(error("Issue on the runtime"))
+    println(warning("Be careful to that"))
+    println(info("Information for the user"))
+    println(data("important data to show"))
+    println(deadly("end of the program because of an error"))
+    println(comment("Comment for the dev"))
+
+    print(error())
+    print(deadly())
+    print(warning())
+    println(data("You can combine the markup"))
+
+}
