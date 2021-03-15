2a13151a428efea7be28a78d668d420aede226f4
kallu <lucas.aries@sfr.fr>
2021-03-15 08:49:39 +0100

    java compatibility

--- src/main/kotlin/Text.kt
+++ src/main/kotlin/Text.kt
@@ -10,15 +10,15 @@ internal class TextString{
         internal const val close = "\u001B[37m] "
     }
 }
-
+@JvmOverloads
 fun error(message: String = "") = "$open\u001B[31mERROR$close$reset$message"
-
+@JvmOverloads
 fun warning(message: String = "") = "$open\u001B[33mWARNING$close$reset$message"
-
+@JvmOverloads
 fun data(message: String = "") = "$open\u001B[35mDATA$close$reset$message"
-
+@JvmOverloads
 fun info(message: String = "") = "$open\u001B[36mINFO$close$reset$message"
-
+@JvmOverloads
 fun deadly(message: String = "") = "$open\u001B[31mDEADLY$close\u001B[31m$message"
-
+@JvmOverloads
 fun comment(message: String = "") = "${open}COMMENT$close$message"
